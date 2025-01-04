5cfe7434db4aa82edc2834e42362b5a4d3e85e43
Nicolas Modrzyk <hellonico@gmail.com>
2025-01-04 18:40:41 +0900

    revert some code change

--- src/linen/history.clj
+++ src/linen/history.clj
@@ -1,12 +1,17 @@
 (ns linen.history
   (:require [clojure.java.io :as io]))
 
-(def history-file-path "file-history.txt")
+
+(def history-file-path (str (System/getProperty "user.home") "/file-history.txt"))
+;(def history-file-path "/tmp/file-history.txt")
 
 (defn read-history []
-  (when-let [lines (line-seq (io/reader history-file-path))]
-    lines))
+  (if (.exists (io/file history-file-path))
+    (with-open [reader (io/reader history-file-path)]
+      (doall (line-seq reader)))
+    []))
 
 (defn append-to-history [file-path]
-  (when (not (read-history))
-    (spit history-file-path (str file-path "\n") :append true)))
+  (let [history (set (read-history))]
+    (when-not (history file-path)
+      (spit history-file-path (str file-path "\n") :append true))))
