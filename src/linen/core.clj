90962d388cce50ef1309ae14f937c536c31be3fd
Nicolas Modrzyk <hellonico@gmail.com>
2025-01-07 00:34:27 +0900

    - simple mode using ESC - preparation for chat mode

--- src/linen/core.clj
+++ src/linen/core.clj
@@ -18,6 +18,7 @@
          :model        "llama3.2"
          :prompt       ""
          :question     ""
+         :simple       false
          :rows         []
          :headers      []
          :local-models []
@@ -90,33 +91,19 @@
            :format content)
     ))
 
+(def spinner-image
+  (Image. (io/input-stream (io/resource "spinner.gif"))))
+
+
 (defn right-panel [state]
   {:fx/type   :v-box
-   ;:h-box/hgrow :always
    :min-width 400.0
    :spacing   5
    :padding   5
    :children  [
-               {:fx/type   :h-box
-                :spacing   5
-                :alignment :center-left
-                :children  [{:fx/type :label
-                             :text    "URL:"}
-                            {:fx/type         :text-field
-                             :text            (:url state)
-                             :on-text-changed #(do
-                                                 (swap! *state assoc :url %)
-                                                 (async/thread (pyjama.state/local-models *state)))}
-                            {:fx/type :label
-                             :text    "Model:"}
-                            {:fx/type          :combo-box
-                             :items            (:local-models state)
-                             :value            (:model state)
-                             :on-value-changed #(swap! *state assoc :model %)}
 
-                            ]
-                }
                {:fx/type         :h-box
+                :spacing         5
                 :alignment       :center-left
                 :on-drag-over    (fn [^DragEvent event]
                                    (let [db (.getDragboard event)]
@@ -124,33 +111,48 @@
                                        (doto event
                                          (.acceptTransferModes (into-array TransferMode [TransferMode/COPY]))))))
                 :on-drag-dropped #(handle-drag-dropped-format *state %)
-                :children        [
-                                  {:fx/type :label
-                                   :text    "Format:"}
-                                  (if (not (nil? (:format-file @*state)))
-                                    {:fx/type  :h-box
-                                     :children [
+                :children
+                (if (:simple state) [] [{:fx/type :label
+                                         :text    "URL:"}
+                                        {:fx/type         :text-field
+                                         :text            (:url state)
+                                         :on-text-changed #(do
+                                                             (swap! *state assoc :url %)
+                                                             (async/thread (pyjama.state/local-models *state)))}
+                                        {:fx/type :label
+                                         :text    "Model:"}
+                                        {:fx/type          :combo-box
+                                         :items            (:local-models state)
+                                         :value            (:model state)
+                                         :on-value-changed #(swap! *state assoc :model %)}
 
-                                                {:fx/type :label
-                                                 :text    (.getName (io/as-file (:format-file @*state)))}
-                                                {:fx/type          :image-view
-                                                 :fit-height       18.0
-                                                 :fit-width        18.0
-                                                 :preserve-ratio   true
-                                                 :image            (Image. (io/input-stream (io/resource "close-40.png")))
-                                                 :on-mouse-clicked (fn [_]
-                                                                     (swap! *state assoc
-                                                                            :format-file nil
-                                                                            :format nil
-                                                                            ))
-                                                 }
-                                                ]
-                                     }
-                                    {:fx/type :label
-                                     :text    "Free format. (Drag an edn file to enforce output.)"}
-                                    )
+                                        {:fx/type :label
+                                         :text    "Format:"}
+                                        (if (not (nil? (:format-file @*state)))
+                                          {:fx/type  :h-box
+                                           :alignment :center-left
+                                           :children [
 
-                                  ]
+                                                      {:fx/type :label
+                                                       :text    (.getName (io/as-file (:format-file @*state)))}
+                                                      {:fx/type          :image-view
+                                                       :fit-height       18.0
+                                                       :fit-width        18.0
+                                                       :preserve-ratio   true
+                                                       :image            (Image. (io/input-stream (io/resource "close-40.png")))
+                                                       :on-mouse-clicked (fn [_]
+                                                                           (swap! *state assoc
+                                                                                  :format-file nil
+                                                                                  :format nil
+                                                                                  ))
+                                                       }
+                                                      ]
+                                           }
+                                          {:fx/type :label
+                                           :text    "Free. (Drag edn file)"}
+                                          )
+
+                                        ])
                 }
                {:fx/type   :h-box
                 :alignment :center-left
@@ -164,10 +166,6 @@
                                                   (swap! *state assoc :prompt (linen.handlers/handle-file-action :prompt *state))
                                                   ;(clojure.pprint/pprint @*state)
                                                   (pyjama.state/handle-submit *state))}
-                            ]}
-               {:fx/type   :h-box
-                :alignment :center-left
-                :children  [
                             {:fx/type :label
                              :text    "Clipboard:"}
                             {:fx/type          :combo-box
@@ -184,47 +182,61 @@
                                                          :freetext (linen.utils/get-clipboard-content)
                                                          :question %)
                                                   (swap! *state assoc :prompt (linen.handlers/handle-file-action :prompt *state))
-                                                  ;(clojure.pprint/pprint @*state)
                                                   (pyjama.state/handle-submit *state))}
                             ]
                 }
 
-               {
-                :fx/type :label
-                :text    "Prompt:"}
-               {:fx/type         :text-area
-                :text            (:question state)
-                :on-text-changed #(do
-                                    (swap! *state assoc :question %)
-                                    (swap! *state assoc :prompt (linen.handlers/handle-file-action :prompt *state)))}
-               (if (not (state :processing))
-                 {:fx/type   :button
-                  :text      "Ask"
-                  :on-action (fn [_] (pyjama.state/handle-submit *state))
-                  }
-                 {:fx/type   :h-box
-                  :alignment :center-left
-                  :children  [
-                              {
-                               :fx/type :label
-                               :text    (str (:model @*state) " is thinking ...")}
-                              {:fx/type   :button
-                               :text      "Stop"
-                               :on-action (fn [_] (swap! *state assoc :processing false))
-                               }
-                              ]
-                  }
-                 )
-               {
-                :fx/type :label
-                :text    "Response:"}
                {:fx/type  :v-box
                 :style    "-fx-background-color: black; -fx-background-radius: 10; -fx-padding: 1;"
                 :children [
-                           {:fx/type linen.components/my-webview
-                            :props   {:html (:response state)}
-                            :desc    {:fx/type :web-view}}
+                           {:fx/type     linen.components/my-webview
+                            :v-box/vgrow :always
+                            :max-height  Double/MAX_VALUE
+                            :props       {:html (:response state)}
+                            :desc        {:fx/type :web-view}}
                            ]}
+
+               {
+                :fx/type  :h-box
+                :children [
+                           {:fx/type         :text-field
+                            ;:style    "-fx-background-color: white; -fx-background-radius: 5; -fx-padding: 1;"
+                            :max-width       Double/MAX_VALUE
+                            :h-box/hgrow     :always
+                            :text            (:question state)
+                            :on-text-changed #(do
+                                                (swap! *state assoc :question %)
+                                                (swap! *state assoc :prompt (linen.handlers/handle-file-action :prompt *state)))}
+                           (if (not (state :processing))
+                             {:fx/type   :button
+                              :text      "Ask"
+                              :on-action (fn [_] (pyjama.state/handle-submit *state))
+                              }
+                             {:fx/type          :image-view
+                              :image            spinner-image
+                              :on-mouse-clicked (fn [_] (swap! *state assoc :processing false))
+                              :fit-width        24
+                              :fit-height       24}
+                             ;{:fx/type   :h-box
+                             ; :alignment :center-left
+                             ; :children  [
+                             ;             ;{
+                             ;             ; :fx/type :label
+                             ;             ; :text    (str (:model @*state) " is thinking ...")}
+                             ;             ;{:fx/type   :button
+                             ;             ; :text      "Stop"
+                             ;             ; :on-action (fn [_] (swap! *state assoc :processing false))
+                             ;             ; }
+                             ;             {:fx/type    :image-view
+                             ;              :image      (Image. (io/input-stream (io/resource "spinner.gif")))
+                             ;              :on-mouse-clicked (fn [_] (swap! *state assoc :processing false))
+                             ;              :fit-width  24
+                             ;              :fit-height 24}
+                             ;             ]
+                             ; }
+                             )
+                           ]}
+
                ]})
 
 (defn app-view [state]
@@ -233,32 +245,38 @@
    :width            1200
    :min-width        800
    :height           800
-   :min-height       800
-   :title            "Pyjama Linen - Query Your Data"
+   :min-height       400
+   :title            "Pyjama Linen"
    :on-close-request (fn [_] (System/exit 0))
-   :icons            [(Image. (io/input-stream (io/resource "delicious.png")))]
-   :scene            {:fx/type     :scene
-                      :stylesheets #{(.getUserAgentStylesheet (NordLight.))}
+   :icons            [(Image. (io/input-stream (io/resource "flax-seeds-96.png")))]
+   :scene            {:fx/type      :scene
+                      :accelerators {[:escape] {:event/type ::simple}}
+                      :stylesheets  #{(.getUserAgentStylesheet (NordLight.))}
                       :root
-                      ;{
-                      ;                  :fx/type  :h-box
-                      ;                  :spacing  10
-                      ;                  :children [
-                      ;                             (left-panel state)
-                      ;                             (right-panel state)
-                      ;                             ]
-                      ;                  }
                       {:fx/type           :split-pane
                        :divider-positions [0.5]             ;; Initial position (50% split)
                        :orientation       :horizontal       ;; Horizontal split
-                       :items             [(left-panel state) (right-panel state)]
+                       :items             (if (:simple state)
+                                            [(right-panel state)]
+                                            [(left-panel state) (right-panel state)])
                        }
                       }})
+
+(defmulti event-handler :event/type)
+
+(defmethod event-handler :default [e]
+  (prn (:event/type e) (:fx/event e) (dissoc e :fx/context :fx/event :event/type)))
+
+(defmethod event-handler ::simple [e]
+  (swap! *state assoc :simple (not (:simple @*state)))
+  )
 
 (def renderer
   (fx/create-renderer
     :middleware (fx/wrap-map-desc assoc :fx/type app-view)
-    :opts {:state *state}))
+    :opts {:state                    *state
+           :fx.opt/map-event-handler event-handler
+           }))
 
 (defn -main []
   (async/thread (pyjama.state/local-models *state))
